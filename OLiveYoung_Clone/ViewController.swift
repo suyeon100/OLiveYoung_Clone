26b784d7d58b0a41ea8d6a4229dbe635029169fb
suyeon100 <xxyx7115@gmail.com>
2024-05-14 13:35:27 +0900

    Initial commit

---
+++ OLiveYoung_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OLiveYoung_Clone
+//
+//  Created by 박수연 on 5/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
